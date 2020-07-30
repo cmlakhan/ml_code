118fde620d62dd120f5ee7b2f674e5e53176db10
Chirag Lakhani <chirag.lakhani@gmail.com>
2020-07-30 13:36:50 -0400

    add updated files

---
+++ d2l_pytorch/chapter_3.py
@@ -0,0 +1,405 @@
+%matplotlib inline
+from d2l import torch as d2l
+import math
+import torch
+import numpy as np
+import time
+import random
+
+
+
+n = 10000
+a = torch.ones(n)
+b = torch.ones(n)
+
+
+
+class Timer:  #@save
+    """Record multiple running times."""
+    def __init__(self):
+        self.times = []
+        self.start()
+
+    def start(self):
+        """Start the timer."""
+        self.tik = time.time()
+
+    def stop(self):
+        """Stop the timer and record the time in a list."""
+        self.times.append(time.time() - self.tik)
+        return self.times[-1]
+
+    def avg(self):
+        """Return the average time."""
+        return sum(self.times) / len(self.times)
+
+    def sum(self):
+        """Return the sum of time."""
+        return sum(self.times)
+
+    def cumsum(self):
+        """Return the accumulated time."""
+        return np.array(self.times).cumsum().tolist()
+
+
+
+c = torch.zeros(n)
+timer = Timer()
+for i in range(n):
+    c[i] = a[i] + b[i]
+f'{timer.stop():.5f} sec'
+
+
+
+
+timer.start()
+d = a + b
+f'{timer.stop():.5f} sec'
+
+
+
+
+
+def normal(x, mu, sigma):
+    p = 1 / math.sqrt(2 * math.pi * sigma**2)
+    return p * np.exp(-0.5 / sigma**2 * (x - mu)**2)
+
+
+
+
+
+# Use numpy again for visualization
+x = np.arange(-7, 7, 0.01)
+
+# Mean and standard deviation pairs
+params = [(0, 1), (0, 2), (3, 1)]
+d2l.plot(x, [normal(x, mu, sigma) for mu, sigma in params], xlabel='x',
+         ylabel='p(x)', figsize=(4.5, 2.5),
+         legend=[f'mean {mu}, std {sigma}' for mu, sigma in params])
+
+
+
+
+
+def synthetic_data(w, b, num_examples):  #@save
+    """Generate y = Xw + b + noise."""
+    X = torch.normal(0, 1, (num_examples, len(w)))
+    y = torch.matmul(X, w) + b
+    y += torch.normal(0, 0.01, y.shape)
+    return X, y.reshape((-1, 1))
+
+true_w = torch.tensor([2, -3.4])
+true_b = 4.2
+features, labels = synthetic_data(true_w, true_b, 1000)
+
+
+print('features:', features[0],'\nlabel:', labels[0])
+
+
+
+d2l.set_figsize()
+# The semicolon is for displaying the plot only
+d2l.plt.scatter(d2l.numpy(features[:, 1]), d2l.numpy(labels), 1);
+
+
+
+
+
+
+
+def data_iter(batch_size, features, labels):
+    num_examples = len(features)
+    indices = list(range(num_examples))
+    # The examples are read at random, in no particular order
+    random.shuffle(indices)
+    for i in range(0, num_examples, batch_size):
+        batch_indices = torch.tensor(
+            indices[i: min(i + batch_size, num_examples)])
+        yield features[batch_indices], labels[batch_indices]
+
+
+
+
+
+
+
+batch_size = 10
+
+for X, y in data_iter(batch_size, features, labels):
+    print(X, '\n', y)
+    break
+
+
+
+
+w = torch.normal(0, 0.01, size=(2,1), requires_grad=True)
+b = torch.zeros(1, requires_grad=True)
+
+
+
+
+def linreg(X, w, b):  #@save
+    """The linear regression model."""
+    return torch.matmul(X, w) + b
+
+
+
+
+def squared_loss(y_hat, y):  #@save
+    """Squared loss."""
+    return (y_hat - y.reshape(y_hat.shape)) ** 2 / 2
+
+
+
+
+def sgd(params, lr, batch_size):  #@save
+    """Minibatch stochastic gradient descent."""
+    for param in params:
+        param.data.sub_(lr*param.grad/batch_size)
+        param.grad.data.zero_()
+
+
+
+
+lr = 0.03
+num_epochs = 30
+net = linreg
+loss = squared_loss
+
+for epoch in range(num_epochs):
+    for X, y in data_iter(batch_size, features, labels):
+        l = loss(net(X, w, b), y)  # Minibatch loss in `X` and `y`
+        # Compute gradient on `l` with respect to [`w`, `b`]
+        l.sum().backward()
+        sgd([w, b], lr, batch_size)  # Update parameters using their gradient
+    with torch.no_grad():
+        train_l = loss(net(features, w, b), labels)
+        print(f'epoch {epoch + 1}, loss {float(train_l.mean()):f}')
+
+
+
+
+print(f'error in estimating w: {true_w - w.reshape(true_w.shape)}')
+print(f'error in estimating b: {true_b - b}')
+
+
+
+
+
+
+#### Simple Version
+
+from d2l import torch as d2l
+import numpy as np
+import torch
+from torch.utils import data
+
+true_w = torch.tensor([2, -3.4])
+true_b = 4.2
+features, labels = d2l.synthetic_data(true_w, true_b, 1000)
+
+
+
+
+def load_array(data_arrays, batch_size, is_train=True):  #@save
+    """Construct a PyTorch data iterator."""
+    dataset = data.TensorDataset(*data_arrays)
+    return data.DataLoader(dataset, batch_size, shuffle=is_train)
+
+batch_size = 10
+data_iter = load_array((features, labels), batch_size)
+
+
+
+next(iter(data_iter))
+
+
+# `nn` is an abbreviation for neural networks
+from torch import nn
+
+##Two inputs and one output
+net = nn.Sequential(nn.Linear(2, 1))
+
+##Initialize weights to random
+net[0].weight.data.normal_(0, 0.01)
+
+## Initialize weights to 0
+net[0].bias.data.fill_(0)
+
+loss = nn.MSELoss()
+
+
+trainer = torch.optim.SGD(net.parameters(), lr=0.03)
+
+
+num_epochs = 3
+for epoch in range(num_epochs):
+    for X, y in data_iter:
+        l = loss(net(X) ,y)
+        trainer.zero_grad()
+        l.backward()
+        trainer.step()
+    l = loss(net(features), labels)
+    print(f'epoch {epoch + 1}, loss {l:f}')
+
+
+w = net[0].weight.data
+print('error in estimating w:', true_w - w.reshape(true_w.shape))
+b = net[0].bias.data
+print('error in estimating b:', true_b - b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+%matplotlib inline
+from d2l import torch as d2l
+import torch
+import torchvision
+from torchvision import transforms
+from torch.utils import data
+
+d2l.use_svg_display()
+
+
+
+
+# `ToTensor` converts the image data from PIL type to 32-bit floating point
+# tensors. It divides all numbers by 255 so that all pixel values are between
+# 0 and 1
+trans = transforms.ToTensor()
+mnist_train = torchvision.datasets.FashionMNIST(
+    root="../data", train=True, transform=trans, download=True)
+mnist_test = torchvision.datasets.FashionMNIST(
+    root="../data", train=False, transform=trans, download=True)
+
+
+
+len(mnist_train), len(mnist_test)
+
+
+mnist_train[0][0].shape
+
+
+
+def get_fashion_mnist_labels(labels):  #@save
+    """Return text labels for the Fashion-MNIST dataset."""
+    text_labels = ['t-shirt', 'trouser', 'pullover', 'dress', 'coat',
+                   'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
+    return [text_labels[int(i)] for i in labels]
+
+
+
+
+def show_images(imgs, num_rows, num_cols, titles=None, scale=1.5):  #@save
+    """Plot a list of images."""
+    figsize = (num_cols * scale, num_rows * scale)
+    _, axes = d2l.plt.subplots(num_rows, num_cols, figsize=figsize)
+    axes = axes.flatten()
+    for i, (ax, img) in enumerate(zip(axes, imgs)):
+        ax.imshow(d2l.numpy(img))
+        ax.axes.get_xaxis().set_visible(False)
+        ax.axes.get_yaxis().set_visible(False)
+        if titles:
+            ax.set_title(titles[i])
+    return axes
+
+
+
+
+
+X, y = next(iter(data.DataLoader(mnist_train, batch_size=18)))
+show_images(X.reshape(18, 28, 28), 2, 9, titles=get_fashion_mnist_labels(y));
+
+
+
+batch_size = 256
+
+def get_dataloader_workers():  #@save
+    """Use 4 processes to read the data."""
+    return 4
+
+train_iter = data.DataLoader(mnist_train, batch_size, shuffle=True,
+                             num_workers=get_dataloader_workers())
+
+
+
+
+timer = d2l.Timer()
+for X, y in train_iter:
+    continue
+f'{timer.stop():.2f} sec'
+
+
+
+
+
+def load_data_fashion_mnist(batch_size, resize=None):  #@save
+    """Download the Fashion-MNIST dataset and then load it into memory."""
+    trans = [transforms.ToTensor()]
+    if resize:
+        trans.insert(0, transforms.Resize(resize))
+    trans = transforms.Compose(trans)
+    mnist_train = torchvision.datasets.FashionMNIST(
+        root="../data", train=True, transform=trans, download=True)
+    mnist_test = torchvision.datasets.FashionMNIST(
+        root="../data", train=False, transform=trans, download=True)
+    return (data.DataLoader(mnist_train, batch_size, shuffle=True,
+                            num_workers=get_dataloader_workers()),
+            data.DataLoader(mnist_test, batch_size, shuffle=False,
+                            num_workers=get_dataloader_workers()))
+
+
+
+
+train_iter, test_iter = load_data_fashion_mnist(32, resize=64)
+for X, y in train_iter:
+    print(X.shape, X.dtype, y.shape, y.dtype)
+    break
+
+
+
+
+
+
+from d2l import torch as d2l
+import torch
+from torch import nn
+
+batch_size = 256
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
+
+
+
+
+# PyTorch does not implicitly reshape the inputs. Thus we define a layer to
+# reshape the inputs in our network
+class Reshape(torch.nn.Module):
+    def forward(self, x):
+        return x.view(-1,784)
+
+net = nn.Sequential(Reshape(), nn.Linear(784, 10))
+
+def init_weights(m):
+    if type(m) == nn.Linear:
+        torch.nn.init.normal_(m.weight, std=0.01)
+
+net.apply(init_weights)
